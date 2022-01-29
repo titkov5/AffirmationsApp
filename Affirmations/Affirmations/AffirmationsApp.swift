241307d9b648d83b632d6a8dd1738b23f50cda00
Ivan Titkov <titkoviv@gmail.com>
2022-01-29 14:26:32 +0400

    initial commit

---
+++ Affirmations/Affirmations/AffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsApp.swift
+//  Affirmations
+//
+//  Created by Ivan Titkov on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
